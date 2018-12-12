82d6c4d811555645e882e0f7ceb811cf11c11d64
Folivi Joshua <joshua.folivi@gmail.com>
2018-12-12 20:24:51 +0100

    Add project files.

---
+++ RestClientConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestClientConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
